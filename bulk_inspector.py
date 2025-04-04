fb93e72bca5d62a4edb64f4f835822debcb89a50
liamharder <56138157+liamharder@users.noreply.github.com>
2025-04-03 21:03:42 -0700

    Implement URL scanning through VirusTotal
    Automatically getting URLs from bulk_extractor outputs is not yet implemented

--- bulk_inspector.py
+++ bulk_inspector.py
@@ -1,6 +1,7 @@
 import json
 import sys
 import requests
+import base64
 
 services = { #List of API-accessible services we can query
     "VirusTotal": ""
@@ -56,3 +57,38 @@ else:
         sys.exit("Exiting program. Please edit APIkeys.json so that it contains all necessary API keys.")
         
 #By now we have a dictionary of API keys we can use to process data
+
+#Given a list of urls, scan each one with VirusTotal
+def urlscan_VirusTotal(urls):
+    results = {}
+    for url in urls:
+        print("Scanning '" + url+"':")
+        #VirusTotal wants a url ID, not the url itself, thankfully it gives us the code to generate the ID
+        url_id = base64.urlsafe_b64encode(url.encode()).decode().strip("=")
+        
+        #It also gives us the code for making the API request
+        APIurl = "https://www.virustotal.com/api/v3/urls/" + url_id
+
+        headers = {
+            "accept": "application/json",
+            "x-apikey": APIkeys["VirusTotal"]
+        }
+
+        response = requests.get(APIurl, headers=headers)
+        results[url] = response.json()
+        code = results[url]["data"]["attributes"]["last_http_response_code"]
+        if (code == 200):
+            print("HTTP response: "+str(code)+" OK")
+        else:
+            print("HTTP response: "+str(code))
+    return results
+    
+    
+test = urlscan_VirusTotal({"yahoo.com"})
+for key in test: 
+    print("VirusTotal security analysis for url " + key)
+    print(str(test[key]["data"]["attributes"]["last_analysis_stats"]["malicious"]) + " scanners marked this url as malicious.")
+    print(str(test[key]["data"]["attributes"]["last_analysis_stats"]["suspicious"]) + " scanners marked this url as suspicious.")
+    print(str(test[key]["data"]["attributes"]["last_analysis_stats"]["harmless"]) + " scanners marked this url as harmless.")
+    print(str(test[key]["data"]["attributes"]["last_analysis_stats"]["undetected"]) + " scanners did not detect this url.")
+    print(str(test[key]["data"]["attributes"]["last_analysis_stats"]["timeout"]) + " scanners timed out attempting to scan this url.")
